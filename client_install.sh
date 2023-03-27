73e0e0b486f81372c7f9f6a47052885a31bf5225
Abinavraj Ganesh Sudhakar <abinavraj10@gmail.com>
2023-03-26 23:39:37 -0500

    rollback

--- client_install.sh
+++ client_install.sh
@@ -1,11 +1,19 @@
-curl -o "/root/cncstartup.sh" https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh;
-chmod +x /root/cncstartup.sh;
-file_path="/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service";
-sed -i "14a\\WantedBy=multi-user.target" "$file_path";
-# sed -i "7s/.*/ExecStart=\/usr\/libexec\/nm-dispatcher && \/root\/cncstartup.sh/" "$file_path";
-sed -i "7s/.*/ExecStartPre=\/usr\/libexec\/nm-dispatcher" "$file_path";
-sed -i "7a\\ExecStart=\/root\/cncstartup.sh/" "$file_path";
-sed -i "7a\\Restart=always" "$file_path";
-sed -i "7a\\User=root" "$file_path";
-sudo systemctl daemon-reload;
-sudo systemctl restart dbus-org.freedesktop.nm-dispatcher
+# curl -o "/root/cncstartup.sh" https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh;
+# chmod +x /root/cncstartup.sh;
+# file_path="/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service";
+# sed -i "14a\\WantedBy=multi-user.target" "$file_path";
+# # sed -i "7s/.*/ExecStart=\/usr\/libexec\/nm-dispatcher && \/root\/cncstartup.sh/" "$file_path";
+# sed -i "7s/.*/ExecStartPre=\/usr\/libexec\/nm-dispatcher/" "$file_path";
+# sed -i "7a\\ExecStart=\/root\/cncstartup.sh/" "$file_path";
+# sed -i "7a\\Restart=always" "$file_path";
+# sed -i "7a\\User=root" "$file_path";
+# sudo systemctl daemon-reload;
+# sudo systemctl restart dbus-org.freedesktop.nm-dispatcher
+
+curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh > /root/cncstartup.sh;
+chmod +x /root/cncstartup.sh
+curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.service > /etc/systemd/system/cncstartup.service;
+
+sudo systemctl daemon-reload
+sudo systemctl enable cncstartup
+sudo systemctl start cncstartup
